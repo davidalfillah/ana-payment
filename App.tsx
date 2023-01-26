4870fef3b72ce90ea2bf5ed39afbd8438f947eb2
davidalfillah <davidalfillahh@gmail.com>
2023-01-26 10:03:41 +0700

    ini adalah project pertama saya

--- App.tsx
+++ App.tsx
@@ -76,7 +76,7 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-            <Text>Hello Word..</Text>
+            <Text>Hello Sayang</Text>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
